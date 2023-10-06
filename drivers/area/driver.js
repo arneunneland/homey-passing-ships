be1fcd89e2584a356e8013b2d414e78538c8b2e0
arneu <arne.unneland@gmail.com>
2023-10-06 21:14:59 +0000

    wip - new registration

--- drivers/area/driver.js
+++ drivers/area/driver.js
@@ -12,6 +12,33 @@ class MyDriver extends Driver {
     this.log('Area Driver has been initialized');
   }
 
+  async onPair(session) {
+    console.log("onPair starting");
+    this.log("onPair starting");
+
+    // Received when a view has changed
+    session.setHandler("showView", async function (viewId) {
+      this.log("View: " + viewId);
+    });
+
+    session.setHandler("my_event", async function (data) {
+      // data is { 'foo': 'bar' }
+      return "Hello!";
+    });
+
+    session.setHandler("showView", async (viewId) => {
+      if (viewId === "area_view") {
+        const data = await session.emit("hello", "Hello to you!");
+        this.log(data); // Hi!
+      }
+    });
+    this.log("handlers registered");
+
+    await session.showView("areaview");
+    await session.done();
+  }
+
+ 
   /**
    * onPairListDevices is called when a user is adding a device
    * and the 'list_devices' view is called.
